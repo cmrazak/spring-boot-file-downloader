14c3953e091774901f7f48f0cdc2f966b11b22e4
cm.razak@gmail.com <cm.razak@gmail.com>
2019-11-04 21:19:20 +0400

    Initial Commit

---
+++ src/test/java/com/kodefab/signage/signageserver/springbootfiledownloader/SpringBootFileDownloaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodefab.signage.signageserver.springbootfiledownloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFileDownloaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
